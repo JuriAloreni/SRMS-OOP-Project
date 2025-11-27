6907de3646f6b403cace640424a80d05d5795ffd
Juri Aloreni <juri3bdullah@gmail.com>
2025-11-27 08:26:37 +0300

    Create abstract Person class with basic attributes

--- src/Person.java
+++ src/Person.java
@@ -1,1 +1,27 @@
+public abstract class Person {
+protected String name;
+protected int ID;
+
+    public Person(String name, int ID) {
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+}
 
